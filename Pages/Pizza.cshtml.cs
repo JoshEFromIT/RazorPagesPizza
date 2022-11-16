04f1c2ee1eeb554dd890b22ae303e1336273e650
Joshua Elkins <Joshua.Elkins@dmv.virginia.gov>
2022-11-16 16:52:02 -0500

    first commit

---
+++ Pages/Pizza.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesPizza.Models;
+using RazorPagesPizza.Services;
+
+namespace RazorPagesPizza.Pages
+{
+    public class PizzaModel : PageModel
+    {
+        public List<Pizza> pizzas = new();
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = new();
+
+        public string GlutenFreeText(Pizza pizza)
+        {
+            return pizza.IsGlutenFree ? "Gluten Free" : "Not Gluten Free";
+        }
+        public string IsOnSaleText(Pizza pizza)
+        {
+            return pizza.IsOnSale ? "On Sale!" : "No Discount Available";
+        }
+        public void OnGet()
+        {
+            pizzas = PizzaService.GetAll();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            PizzaService.Add(NewPizza);
+            return RedirectToAction("Get");
+        }
+        public IActionResult OnPostDelete(int id)
+        {
+            PizzaService.Delete(id);
+            return RedirectToAction("Get");
+        }
+
+
+    }
+}
